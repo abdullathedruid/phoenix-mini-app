bba4e55ef845f5af2401a3669300689c12d5b511
abdullathedruid <abdulla.alkamil@gmail.com>
2025-08-23 23:14:32 +0100

    CSP for miniapp

--- lib/miniapp_web/router.ex
+++ lib/miniapp_web/router.ex
@@ -7,7 +7,9 @@ defmodule MiniappWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {MiniappWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers,%{
+      "content-security-policy" => "default-src 'self'; img-src 'self' data: https:; script-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors *"
+    }
   end
 
   pipeline :api do
