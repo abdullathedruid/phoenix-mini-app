aaa18f3e1207018cb9ec9e6ad10bcbfac648d9da
abdullathedruid <abdulla.alkamil@gmail.com>
2025-08-24 00:22:28 +0100

    live state

--- lib/miniapp_web/live/wallet.ex
+++ lib/miniapp_web/live/wallet.ex
@@ -3,12 +3,27 @@ defmodule MiniappWeb.WalletLive do
   use MiniappWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket
+  |> assign(:user_fid, nil)
+  |> assign(:user_display_name, nil)
+  |> assign(:user_pfp_url, nil)
+  }
   end
 
   def handle_event("wallet:connect", %{"context" => context}, socket) do
-    Logger.info("Wallet connected: #{inspect(context)}")
-    {:noreply, socket}
+    %{"user" => %{"fid" => user_fid} = user,
+    "client" => %{"clientFid" => client_fid},
+    } = context
+
+    user_display_name = user |> Map.get("displayName")
+    user_pfp_url = user |> Map.get("pfpUrl")
+
+    {:noreply, socket
+    |> assign(:user_fid, user_fid)
+    |> assign(:user_display_name, user_display_name)
+    |> assign(:user_pfp_url, user_pfp_url)
+    |> assign(:client_fid, client_fid)
+    }
   end
 
   def handle_event(event, unsigned_params, socket) do
