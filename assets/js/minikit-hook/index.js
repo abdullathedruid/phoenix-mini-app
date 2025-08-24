ca68bfc5405054eb9c29f3a5ccaa03f524243576
abdullathedruid <abdulla.alkamil@gmail.com>
2025-08-24 15:30:46 +0100

    lets try some stuff

--- assets/js/minikit-hook/index.js
+++ assets/js/minikit-hook/index.js
@@ -1,4 +1,11 @@
 import { sdk } from "@farcaster/miniapp-sdk";
+import { createWalletClient, custom } from "viem";
+import { base } from "viem/chains";
+
+const walletClient = createWalletClient({
+  chain: base,
+  transport: custom(window.ethereum),
+});
 
 /**
  * @type {import("phoenix_live_view").Hook}
@@ -13,6 +20,29 @@ const WalletHook = {
       }
     } catch (error) {
       console.error("MiniApp SDK initialization failed", error);
+    }
+
+    try {
+      const provider = await sdk.wallet.getEthereumProvider();
+      if (provider) {
+        this.pushEvent("miniapp:provider", { provider });
+      }
+    } catch (error) {
+      console.error("Failed to get Ethereum provider", error);
+    }
+
+    try {
+      const block = await window.publicClient.getBlockNumber();
+      this.pushEvent("miniapp:block", { blockNumber: block });
+    } catch (error) {
+      console.error("Failed to get block number", error);
+    }
+
+    try {
+      const account = await walletClient.getAddresses();
+      this.pushEvent("miniapp:account", { account: account[0] });
+    } catch (error) {
+      console.error("Failed to get account", error);
     }
   }
 }
