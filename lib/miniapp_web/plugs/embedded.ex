f84b6fd2828e8031d2e90e46923d2b07f4bd6da4
abdullathedruid <abdulla.alkamil@gmail.com>
2025-08-24 20:36:07 +0100

    allow inline script

--- lib/miniapp_web/plugs/embedded.ex
+++ lib/miniapp_web/plugs/embedded.ex
@@ -7,7 +7,7 @@ defmodule MiniappWeb.Plugs.Embedded do
     conn
     |> put_resp_header(
       "content-security-policy",
-      "default-src 'self'; img-src 'self' data: https:; script-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors *"
+      "default-src 'self'; img-src 'self' data: https:; script-src 'self' 'sha256-ZSLtwbmogvdRQWylw6MDGKCK+VIz+hyMBvfpcdn8AQs='; style-src 'self' 'unsafe-inline'; frame-ancestors *"
     )
     |> delete_resp_header("x-frame-options")
   end
